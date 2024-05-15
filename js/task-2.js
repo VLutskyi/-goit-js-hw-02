24d1b4550b50a22063b78c02827114ad0570a6dd
Viktor_Lutskyi <158850898+VLutskyi@users.noreply.github.com>
2024-05-15 22:18:39 +0300

    update task-2

--- js/task-2.js
+++ js/task-2.js
@@ -2,10 +2,10 @@ function formatMessage(message, maxLength) {
   if (message.length <= maxLength) {
     return message;
   } else {
-    const truncatedMessage = message.substring(0, maxLength - 3) + "...";
-    return truncatedMessage;
+    return message.slice(0, maxLength) + "...";
   }
 }
+
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
 console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
